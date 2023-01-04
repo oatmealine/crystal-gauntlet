87342acee07be8e40cb35fe957aa80cd44c7fbbb
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2023-01-04 12:49:34 +0300

    i hate rebase!! i hate rebase!

--- src/template_endpoints/song_search.cr
+++ src/template_endpoints/song_search.cr
@@ -2,7 +2,7 @@ require "ecr"
 
 include CrystalGauntlet
 
-CrystalGauntlet.template_endpoints["/tools/song_search"] = ->(context : HTTP::Server::Context): String {
+CrystalGauntlet.template_endpoints["/tools/song_search"] = ->(context : HTTP::Server::Context) {
   error = nil
   songs = nil
   result_limit = 10
@@ -13,11 +13,11 @@ CrystalGauntlet.template_endpoints["/tools/song_search"] = ->(context : HTTP::Se
       query = "%#{params["query"]}%"
       songs = DATABASE.query_all("select song_data.id, song_authors.name, song_data.name from song_data join song_authors on song_authors.id = song_data.author_id where song_data.id = ? or song_authors.name like ? or song_data.name like ? limit #{result_limit}", params["query"], query, query, as: {Int32, String, String})
     rescue error
-      ECR.render("./public/template/song_search.ecr")
+      ECR.embed("./public/template/song_search.ecr", context.response)
     else
-      ECR.render("./public/template/song_search.ecr")
+      ECR.embed("./public/template/song_search.ecr", context.response)
     end
+  else
+    ECR.embed("./public/template/song_search.ecr", context.response)
   end
-
-  ECR.render("./public/template/song_search.ecr")
 }
