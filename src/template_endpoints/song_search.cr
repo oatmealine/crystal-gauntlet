7d84b1ddd21236a277b7a4b874414c49f3daae43
winter <wint0r@noreply.oat.zone>
2023-01-05 11:53:50 +0100

    remove limit

--- src/template_endpoints/song_search.cr
+++ src/template_endpoints/song_search.cr
@@ -7,13 +7,12 @@ CrystalGauntlet.template_endpoints["/tools/song_search"] = ->(context : HTTP::Se
 
   error = nil
   songs = nil
-  result_limit = 10
   body = context.request.body
   if body
     begin
       params = URI::Params.parse(body.gets_to_end)
       query = "%#{params["query"]}%"
-      songs = DATABASE.query_all("select song_data.id, song_authors.name, song_data.name from song_data join song_authors on song_authors.id = song_data.author_id where song_data.id = ? or song_authors.name like ? or song_data.name like ? limit #{result_limit}", params["query"], query, query, as: {Int32, String, String})
+      songs = DATABASE.query_all("select song_data.id, song_authors.name, song_data.name from song_data join song_authors on song_authors.id = song_data.author_id where song_data.id = ? or song_authors.name like ? or song_data.name like ?", params["query"], query, query, as: {Int32, String, String})
     rescue error
       ECR.embed("./public/template/song_search.ecr", context.response)
     else
