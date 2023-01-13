095c3123ab8fd6ac38dce852b8b6986a1667def2
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2023-01-13 20:57:09 +0300

    dynamically update icon on account management page w/ user's icon
    i made an api for this :)

--- src/template_endpoints/account_management.cr
+++ src/template_endpoints/account_management.cr
@@ -12,7 +12,10 @@ CrystalGauntlet.template_endpoints["/accounts"] = ->(context : HTTP::Server::Con
   username = nil
   Templates.auth()
 
-  stars, demons, coins, user_coins, diamonds, creator_points = DATABASE.query_one("select stars, demons, coins, user_coins, diamonds, creator_points from users where id = ?", user_id, as: {Int32, Int32, Int32, Int32, Int32, Int32})
+  stars, demons, coins, user_coins, diamonds, creator_points, icon_type, color1, color2, cube, ship, ball, ufo, wave, robot, spider, glow = DATABASE.query_one("select stars, demons, coins, user_coins, diamonds, creator_points, icon_type, color1, color2, cube, ship, ball, ufo, wave, robot, spider, glow from users where id = ?", user_id, as: {Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32})
+
+  icon_value = [cube, ship, ball, ufo, wave, robot, spider][icon_type]
+  type_str = ["cube", "ship", "ball", "ufo", "wave", "robot", "spider"][icon_type]
 
   ECR.embed("./public/template/account_management.ecr", context.response)
 }
